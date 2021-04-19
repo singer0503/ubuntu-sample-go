03cf5c5b1763555990f23cdc6351f4e7c0fa0be6
黃立豪 <singer0503@gmail.com>
2021-04-19 13:23:57 +0800

    下載 sample

--- 01-http-server-sample/main.go
+++ 01-http-server-sample/main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 // 測試主機是否有回應
 func hello(w http.ResponseWriter, req *http.Request) {
 
-    fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "hello\n")
 }
 
 // 回傳主機看到的 headers
 func headers(w http.ResponseWriter, req *http.Request) {
 
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func downloadFileSample(w http.ResponseWriter, r *http.Request) {
+	file := "FileZilla_3.53.1_win64-setup.exe"
+	fmt.Println("in call : " + file)
+	// 設定此 Header 告訴瀏覽器下載檔案。 如果沒設定則會在新的 tab 開啟檔案。
+	w.Header().Set("Content-Disposition", "attachment; filename="+file)
+
+	http.ServeFile(w, r, file)
 }
 
 func main() {
-	port := ":80"
-    http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
-
+	port := ":8090"
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/download", downloadFileSample)
 	fmt.Println("ok")
-	fmt.Println("port = "+port)
-    http.ListenAndServe(port, nil)
-	
+	fmt.Println("port = " + port)
+	http.ListenAndServe(port, nil)
+
 }
---
+++ 02-http-return-stream/main.go
@@ -0,0 +1,76 @@
+// Start a mock HTTP server that returns 2GB of data in the response. Make a
+// HTTP request to this server and print the amount of data read from the
+// response.
+package main
+
+import (
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "strings"
+    "time"
+)
+
+const oneMB = 1024 * 1024
+const oneGB = 1024 * oneMB
+const responseSize = 2 * oneGB
+
+const serverAddr = "localhost:9999"
+
+func startServer() {
+    // Mock HTTP server that always returns 2GB of data
+    go http.ListenAndServe(serverAddr, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+        w.Header().Set("Content-length", fmt.Sprintf("%d", responseSize))
+
+        // 1MB buffer that'll be copied multiple times to the response
+        buf := []byte(strings.Repeat("x", oneMB))
+
+        for i := 0; i < responseSize/len(buf); i++ {
+            if _, err := w.Write(buf); err != nil {
+                log.Fatal("Failed to write to response. Error: ", err.Error())
+            }
+        }
+    }))
+
+    // Some grace period for the server to start
+    time.Sleep(100 * time.Millisecond)
+}
+
+func main() {
+    startServer()
+
+    // HTTP client
+    req, err := http.NewRequest("GET", "http://"+serverAddr, nil)
+    if err != nil {
+        log.Fatal("Error creating HTTP request: ", err.Error())
+    }
+
+    client := http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Fatal("Error making HTTP request: ", err.Error())
+    }
+
+    // Read the response header
+    fmt.Println("Response: Content-length:", resp.Header.Get("Content-length"))
+
+    bytesRead := 0
+    buf := make([]byte, oneMB)
+
+    // Read the response body
+    for {
+        n, err := resp.Body.Read(buf)
+        bytesRead += n
+
+        if err == io.EOF {
+            break
+        }
+
+        if err != nil {
+            log.Fatal("Error reading HTTP response: ", err.Error())
+        }
+    }
+
+    fmt.Println("Response: Read", bytesRead, "bytes")
+}
