f9845b63cd198f0beed37305e1e31b9aad60dae9
黃立豪 <singer0503@gmail.com>
2021-05-03 10:16:43 +0800

    更新

--- 06-https-sample/main.go
+++ 06-https-sample/main.go
@@ -28,12 +28,21 @@ func GetHeaders(c *gin.Context) {
 	}
 }
 
+func GetDownloadFileSample(c *gin.Context) {
+	fileName := "FileZilla_3.53.1_win64-setup.exe"
+	// 設定此 Header 告訴瀏覽器下載檔案。 如果沒設定則會在新的 tab 開啟檔案。
+	c.Writer.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileName)) //fmt.Sprintf("attachment; filename=%s", filename) Downloaded file renamed
+	c.Writer.Header().Add("Content-Type", "application/octet-stream")
+	c.File(fileName)
+}
+
 func main() {
 	port := ":8443"
 	router := gin.Default()
 
 	router.GET("/hello", GetHello)
 	router.GET("/headers", GetHeaders)
+	router.GET("/download", GetDownloadFileSample)
 
 	//logrus.Fatal(router.RunTLS(port, "server.crt", "server.key"))
 	logrus.Fatal(router.RunTLS(port, "maxhuang_me.crt", "myserver.key"))
